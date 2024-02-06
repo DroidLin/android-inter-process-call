7f35e884d089976a90fb4e6cd74c2848e91c1f8b
liuzhongao <liuzhongao@corp.netease.com>
2024-02-06 16:20:46 +0800

    proguard.

--- library/src/main/java/com/lza/android/inter/process/library/interfaces/GeneratedStubFunction.kt
+++ library/src/main/java/com/lza/android/inter/process/library/interfaces/GeneratedStubFunction.kt
@@ -4,7 +4,7 @@ package com.lza.android.inter.process.library.interfaces
  * @author liuzhongao
  * @since 2024/2/6 11:26
  */
-interface GeneratedStubFunction {
+interface GeneratedStubFunction : IPCNoProguard {
 
     fun invokeNonSuspendFunction(functionName: String, functionParameter: List<Any?>): Any?
 
