064708a7525547afbc9cf5be0bf71c546c29f471
liuzhongao <1047771038@qq.com>
2024-02-07 00:52:17 +0800

    annotation module

--- annotation/build.gradle.kts
+++ annotation/build.gradle.kts
@@ -1,9 +1,32 @@
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
+    id("maven-publish")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("java") {
+            groupId = "com.github.DroidLin"
+            artifactId = "android-inter-process-call-annotation"
+            version = "1.0.0"
+
+            afterEvaluate {
+                from(components["java"])
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "repositoryLocalRepo"
+            url = uri("${rootProject.projectDir}/repo")
+        }
+    }
 }
